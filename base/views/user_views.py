30f38cc3e37657528ff5e9631602fb3f88f71fec
Ryan Taheri <sailantht@gmail.com>
2021-06-26 18:51:34 +0430

    26.URL's & Views

--- base/views/user_views.py
+++ base/views/user_views.py
@@ -1,6 +1,4 @@
 from django.contrib.auth.hashers import make_password
-from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
@@ -8,9 +6,7 @@ from rest_framework import status
 
 from django.contrib.auth.models import User
 
-from .products import products
-from .models import Product
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializers import UserSerializer, UserSerializerWithToken
 
 # Customize token for sending more information
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -71,15 +67,3 @@ def get_users(request):
     return Response(serializer.data)
 
 
-@api_view(['GET'])
-def get_products(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def get_product(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
-    return Response(serializer.data)
